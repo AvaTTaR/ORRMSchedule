0d9e2baad98203825f76defaab913356ee946dc1
Pavel Eryomin <peryomin.dev@gmail.com>
2017-05-01 23:24:22 +0300

    Added login modal.

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 """ORRM Schedule application."""
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, session, request, redirect, \
+    url_for
 from database import get_this_week_for_site, get_schedule_for_mobile
-# from flask_bootstrap import Bootstrap
 from util import Util
 
 app = Flask(__name__)
-# Bootstrap(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def get_page():
-    """Return."""
+    """Default page."""
+    if request.method == 'POST':
+        if request.form['password'] == 'Mtbank_1':
+            print(request.form['password'])
+            return jsonify({'status': 'OK'})
+        else:
+            return jsonify({'status': '404'})
     return render_template('index.html', dates_this_week=Util.get_this_week(),
                            this_week=get_this_week_for_site())
 
